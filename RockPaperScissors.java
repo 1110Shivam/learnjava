48489c642ac7008f3091eb7d65f1cfef919608a2
Shivam <forgetpassward4@gmail.com>
2022-03-20 17:40:56 +0530

    Rock Paper Scissors game

---
+++ RockPaperScissors.java
@@ -0,0 +1,22 @@
+package LearnJava;
+import java.util.*;
+
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int com = r.nextInt(1,4);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1 for rock, 2 for paper, 3 for scissors = ");
+        int user = sc.nextInt();
+
+        if((user == 1 && com == 1)||(user == 2 && com == 2)||(user == 3 && com == 3)){
+            System.out.println("Game drawn.");;
+        }
+        else if((user == 1 && com == 2)||(user == 2 && com == 3)||(user == 3 && com == 1)){
+            System.out.println("Computer Wins.");
+        }
+        else
+            System.out.println("You Win.");
+        System.out.println("computer entered = " + com);
+    }
+}
