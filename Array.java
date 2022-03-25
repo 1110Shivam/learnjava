95636c094e94f4a049a35dcb4a663efe443db525
Shivam <forgetpassward4@gmail.com>
2022-03-25 18:38:43 +0530

    Syntax to create array in java

--- Array.java
+++ Array.java
@@ -2,6 +2,23 @@ package LearnJava;
 
 public class Array {
     public static void main(String[] args){
-        System.out.println("I'm Software Eng");
+        /*
+        1st way
+        int [] marks;
+        marks = new int[5];
+        */
+        int [] marks = new int[5]; //2nd way
+        marks[0] = 100;
+        marks[1] = 130;
+        marks[2] = 20;
+        marks[3] = 40;
+        marks[4] = 10;
+
+        System.out.println(marks[3]);
+
+        //3rd way
+
+        String [] student = {"Student1","Student2","Student3","Student4"};
+        System.out.println(student[3]);
     }
 }
